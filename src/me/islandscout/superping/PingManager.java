a2c1d7ff5119a0b03be81218168ff84ecf13efc9
Islandscout <islandscout8@gmail.com>
2020-01-17 11:04:22 -0700

    Handle hackers, laggers, and quitters

--- src/me/islandscout/superping/PingManager.java
+++ src/me/islandscout/superping/PingManager.java
@@ -5,12 +5,12 @@ import net.minecraft.server.v1_8_R3.PacketDataSerializer;
 import net.minecraft.server.v1_8_R3.PacketPlayInKeepAlive;
 import net.minecraft.server.v1_8_R3.PacketPlayOutKeepAlive;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.io.IOException;
 import java.util.*;
@@ -25,6 +25,7 @@ public class PingManager implements Listener {
     //do the same, but offset it by some great amount of
     //time to guarantee that we won't have a collision.
     private static final int OFFSET_SECONDS = -500;
+    private static final int TIMEOUT_TICKS = 600;
 
     private final SuperPing plugin;
     private final PacketListener pListener;
@@ -110,6 +111,9 @@ public class PingManager implements Listener {
             } else if (pair.getKey() == id) {
                 ping = (int) ((currTime - pair.getValue()) / 1000000);
                 foundResponse = true;
+                if(i != 0) { //client made goof-up and is out of order
+                    p.kickPlayer("Invalid ping response");
+                }
             }
         }
 
@@ -129,6 +133,10 @@ public class PingManager implements Listener {
         UUID uuid = p.getUniqueId();
         List<Pair<Integer, Long>> pendingPings = pendingPingsMap.getOrDefault(uuid, new ArrayList<>());
 
+        if(pendingPings.size() >= TIMEOUT_TICKS) {
+            p.kickPlayer("Timed out");
+        }
+
         int id = (int)((System.nanoTime() / 1000000L) + (OFFSET_SECONDS * 1000));
         Pair<Integer, Long> pair = new Pair<>(id, 0L); //set to 0 for now
         pendingPings.add(pair);
@@ -145,6 +153,14 @@ public class PingManager implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         pListener.add(e.getPlayer());
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        UUID uuid = e.getPlayer().getUniqueId();
+        pendingPingsMap.remove(uuid);
+        lastPacketTimeMap.remove(uuid);
+        pingMap.remove(uuid);
     }
 
     PacketListener getPacketListener() {
